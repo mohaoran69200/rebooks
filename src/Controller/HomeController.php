029e08cd7edca263e41b7f4cc150c6e89db76d63
mohaoran69200 <mo.gounani@gmail.com>
2025-04-15 17:13:04 +0200

    Affichage des livres en bdd sur la page d'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BookRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
+        $books = $bookRepository->findBy([], ['id' => 'DESC'], 4);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'books' => $books,
         ]);
     }
 }
